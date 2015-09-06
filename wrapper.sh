3a7873e3764d1e9a17b4928bc90d0da2a46b808c
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2015-09-06 16:01:23 -0500

    fix typo

--- wrapper.sh
+++ wrapper.sh
@@ -3,7 +3,7 @@
 fusermount -uz /cfg
 /gocfs -d -db cassandra /cfg
 N=0
-while ! df /cfg | grep gocfs && [ $N < 15 ]; do echo "Waiting to cassandra-fs to come up..."; N=$(( $N + 1 )); sleep 1; done
+while ! df /cfg | grep gocfs && [ $N -lt 15 ]; do echo "Waiting to cassandra-fs to come up..."; N=$(( $N + 1 )); sleep 1; done
 if [ $N -eq 15 ]; then exit 2; fi
 
 if [ -z "$*" ]; then
